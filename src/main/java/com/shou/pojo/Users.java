48786b2fe9f3e2323cffc3ab4ce7be9892b10511
thinker <iam.thinker@foxmail.com>
2022-12-25 13:54:11 +0800

    lombok注解简化实体类部分方法生成

--- src/main/java/com/shou/pojo/Users.java
+++ src/main/java/com/shou/pojo/Users.java
@@ -1,52 +1,17 @@
 package com.shou.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author 11937
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Users {
     private String account;
     private String password;
     private String identity;
-
-    public Users(String account, String password, String identity) {
-        this.account = account;
-        this.password = password;
-        this.identity = identity;
-    }
-
-    public Users() {
-    }
-
-    @Override
-    public String toString() {
-        return "Users{" +
-                "account='" + account + '\'' +
-                ", password='" + password + '\'' +
-                ", identity='" + identity + '\'' +
-                '}';
-    }
-
-    public String getAccount() {
-        return account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getIdentity() {
-        return identity;
-    }
-
-    public void setIdentity(String identity) {
-        this.identity = identity;
-    }
 }
